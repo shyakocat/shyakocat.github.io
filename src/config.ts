251b356d57c39c12482e9014e4e8bc241309a5f9
shyakocat <shyakocat@qq.com>
2026-01-06 12:43:53 +0800

    Friend Link

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/fll.jpeg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "/fll.jpeg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "shyakocat",
 	bio: "The niece of time.",
 	links: [
